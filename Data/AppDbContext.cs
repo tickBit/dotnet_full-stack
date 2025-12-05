a7199b168b33b237f9966d0dede65a4eb3d00e4c
tickBit <marjukosk@gmail.com>
2025-12-05 21:36:22 +0200

    Delete user's records when deleting user's account

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,4 +8,15 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<AppUser> Users => Set<AppUser>();
     public DbSet<Info> Infos => Set<Info>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Info>()
+            .HasOne(i => i.User)
+            .WithMany(u => u.Infos)
+            .HasForeignKey(i => i.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -5,4 +5,7 @@ public class AppUser
     public int Id { get; set; }
     public string Email { get; set; } = "";
     public string PasswordHash { get; set; } = "";
+
+    // Navigation collection
+    public List<Info> Infos { get; set; } = new();
 }
