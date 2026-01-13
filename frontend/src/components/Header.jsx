724af9a9f66636f956b8f8c78238a805674a6b46
tickBit <marjukosk@gmail.com>
2026-01-13 17:37:03 +0200

    Create menu with items for the user

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,47 @@
 import { Link } from 'react-router';
 import { useAuth } from '../contexts/AuthContext';
+import React from 'react';
 
 const Header = () => {
+
     
     const { isLoggedIn, logout } = useAuth();
     
+    const [open, setOpen] = React.useState(false);
+    
+    // if clicked outside navbar in the app's window, hide menu items
+    try {
+        window.document.getElementsByClassName("page")[0].addEventListener("click", function(e) {
+            if (open) setOpen(false);
+        });
+    }
+    catch(err) {
+        // catch error, if the "page" class isn't rendered yet...  
+    }
+    
     return (
-        
-        <div className="navbar">
+        <>
+        <nav className="navbar">
             <Link to="/">Home</Link>
             {isLoggedIn === false ? <>
                      <Link to="/login">Login</Link>
                      <Link to="/register">Register</Link>
                      </>
             : <>
-                <Link onClick={() => logout()}>Logout</Link>
+                <Link onClick={() => setOpen( () => setOpen(!open) ) }>My account</Link>
+                {open && <>
+                <nav className='menu'>
+                <ul>
+                    <li><Link to="/profile">Profile</Link></li>
+                    <li><Link onClick={() => logout()}>Logout</Link></li>
+                </ul>
+                </nav>
+                </>
+                }
                 </>
             }
-            <Link to="/profile">Profile</Link>
-        </div>
+        </nav>
+        </>
     );
 }
 
